93aec8b425fa089c88dfebca9a552f7a8a081753
PolarisCr <kimek12@gmail.com>
2017-04-22 00:02:19 +0900

    170422 Baekjoon 1932

---
+++ 1932/1932.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int arr[501][501] = { 0 };
+int maxarr[501][501] = { 0 };
+queue <pair <int, int> > qu;
+
+int main()
+{
+	int x;
+	cin >> x;
+	for (int i = 1; i <= x; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 1; i <= x; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			if (j == 1)
+				maxarr[i][j] = maxarr[i - 1][1] + arr[i][j];
+
+			else if (j == i)
+				maxarr[i][j] = maxarr[i - 1][j - 1] + arr[i][j];
+
+			else
+			{
+				maxarr[i][j] = max(maxarr[i - 1][j - 1], maxarr[i - 1][j]) + arr[i][j];
+			}
+		}
+	}
+
+	int maxval = -10000;
+	for (int i = 1; i <= x; i++)
+	{
+		if (maxval < maxarr[x][i])
+			maxval = maxarr[x][i];
+	}
+	cout << maxval << endl;
+	return 0;
+}
